0b706a4edb79d9dd61a29f907672b434edbfa0ec
Cameliuu <calin.iacob@student.upt.ro>
2022-10-23 17:00:57 +0300

    Add banner

--- huffman-coding/Program.cs
+++ huffman-coding/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-tem       plate for more information
 
 using huffman_coding.Manager;
-
+using Figgle;
 TextManager textManager = new TextManager();
-
+Console.WriteLine(FiggleFonts.Banner3.Render("Huffman Encoding"));
 textManager.Run();
